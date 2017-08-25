8443e5b8e58c1c73bdc08ff4222006190acbddb7
gfox <gfox@backflipstudios.com>
2017-08-25 15:48:55 -0600

    First commit

---
+++ Assets/Scripts/Flocking/Driver.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using System.Linq;
+
+namespace Flocking
+{
+	using Rand = UnityEngine.Random;
+
+	public delegate void Socializer(Driver driver, ref HashSet<Driver> friends, ref HashSet<Driver> flock);
+
+	public class Driver : MonoBehaviour
+	{
+		public Vector2 Position
+		{
+			get
+			{
+				return new Vector2(transform.position.x, transform.position.z);
+			}
+			set
+			{
+				transform.position = new Vector3(value.x, transform.position.y, value.y);
+			}
+		}
+
+		public Vector2 Velocity { get; private set; }
+
+		private void Awake()
+		{
+			socialFrame = Rand.Range(0, 5); // [0,5)
+		}
+
+		public void Initialize(float simluationScale, float boundsRadiusSq, Socializer socializer)
+		{
+			maxSpeed = simluationScale * 1.0f;
+			wanderWeight = simluationScale * 0.25f;
+			accordanceWeight = simluationScale * 0.05f;
+
+			this.boundsRadiusSq = boundsRadiusSq * 95.0f;
+			this.socializer = socializer;
+		}
+
+		public void Step()
+		{
+			StepSocializing();
+			StepFlocking();
+		}
+
+		private void StepSocializing()
+		{
+			socialFrame = (socialFrame + 1) % 5;
+			if (socialFrame == 0)
+			{
+				socializer(this, ref friends, ref flock);
+			}
+		}
+
+		private void StepFlocking()
+		{
+			Vector2 flockDirection = GetFlockDirection();
+			Vector2 avoidDirection = GetAvoidDirection();
+			Vector2 accordance = GetAccordance();
+			Vector2 bounds = GetBounds();
+			Vector2 wander = GetWander();
+
+			Velocity = Velocity + flockDirection + avoidDirection + accordance + bounds + wander;
+			Velocity = Vector2.ClampMagnitude(Velocity, maxSpeed);
+			Position += Velocity;
+		}
+
+		/// <summary>Helps align with the flock.</summary>
+		private Vector2 GetFlockDirection()
+		{
+			int count = 0;
+			Vector2 average = Vector2.zero;
+			foreach (var friend in flock.Concat(friends))
+			{
+				var dist = (friend.Position - Position).magnitude;
+				average += (friend.Velocity.normalized / dist);
+				++count;
+			}
+
+			average = (count > 1) ? (average / count) : average;
+			return average;
+		}
+
+		/// <summary>Helps prevent a flock from clumping.</summary>
+		private Vector2 GetAvoidDirection()
+		{
+			int count = 0;
+			Vector2 average = Vector2.zero;
+			foreach (var friend in friends)
+			{
+				Vector2 difference = Position - friend.Position;
+				average += difference.normalized / difference.magnitude;
+				++count;
+			}
+
+			average = (count > 1) ? (average / count) : average;
+			return average;
+		}
+
+		/// <summary>Helps a flock stay together.</summary>
+		private Vector2 GetAccordance()
+		{
+			int count = 0;
+			Vector2 average = Vector2.zero;
+			foreach (var friend in flock.Concat(friends))
+			{
+				average += friend.Position;
+				++count;
+			}
+
+			if (count == 0)
+			{
+				return average;
+			}
+			else
+			{
+				average = average / count;
+				Vector2 direction = average - Position;
+				direction.Normalize();
+				return direction * accordanceWeight;
+			}
+		}
+			
+		/// <summary>Ensures we dont leave the system.</summary>
+		private Vector2 GetBounds()
+		{
+			var differenceSq = Position.sqrMagnitude - boundsRadiusSq;
+			if (differenceSq > 0)
+			{
+				return -(Position).normalized * (float)Math.Sqrt(differenceSq) * 0.005f;
+			}
+			else
+			{
+				return Vector2.zero;
+			}
+		}
+
+		/// <summary>I am an individual!</summary>
+		private Vector2 GetWander()
+		{
+			return new Vector2(
+				Rand.Range(-wanderWeight, wanderWeight),
+				Rand.Range(-wanderWeight, wanderWeight));
+		}
+
+		private int socialFrame;
+
+		private float maxSpeed;
+		private float wanderWeight;
+		private float accordanceWeight;
+		private float boundsRadiusSq;
+
+		private Socializer socializer;
+
+		private HashSet<Driver> friends = new HashSet<Driver>();
+		private HashSet<Driver> flock = new HashSet<Driver>();
+	}
+}
---
+++ Assets/Scripts/Flocking/Stage.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Flocking
+{
+	public class Stage : MonoBehaviour
+	{
+		[SerializeField] private GameObject driverPrefab;
+
+		[SerializeField] private float simulationScale;
+		private float flockRadiusSq;
+		private float friendRadiusSq;
+		private float boundsRadiusSq;
+
+		private HashSet<Driver> drivers = new HashSet<Driver>();
+		private HashSet<GameObject> avoidables = new HashSet<GameObject>();
+
+		private void Awake()
+		{
+			var flockRadius = simulationScale * 60.0f;
+			var friendRadius = flockRadius / 1.3f;
+			var boundsRadius = simulationScale * 10.0f;
+
+			flockRadiusSq = flockRadius * flockRadius;
+			friendRadiusSq = friendRadius * friendRadius;
+			boundsRadiusSq = boundsRadius * boundsRadius;
+
+			InstantiateDriver(new Vector2(0.0f, 0.0f));
+			InstantiateDriver(new Vector2(1.0f, 1.0f));
+			InstantiateDriver(new Vector2(2.0f, 2.0f));
+			InstantiateDriver(new Vector2(3.0f, 3.0f));
+			InstantiateDriver(new Vector2(4.0f, 4.0f));
+			InstantiateDriver(new Vector2(5.0f, 5.0f));
+		}
+
+		private void Update()
+		{
+			StepAllDrivers();
+		}
+
+		private void StepAllDrivers()
+		{
+			foreach (var driver in drivers)
+			{
+				driver.Step();
+			}	
+		}
+
+		private void InstantiateDriver(Vector2 position)
+		{
+			var newObject = GameObject.Instantiate<GameObject>(
+				driverPrefab,
+				new Vector3(position.x, 0.0f, position.y),
+				Quaternion.identity,
+				this.transform);
+			
+			var driver = newObject.GetComponent<Driver>();
+			driver.Initialize(simulationScale, boundsRadiusSq, RefreshNeighbors);
+			drivers.Add(driver);
+		}
+
+		private void RefreshNeighbors(Driver driver, ref HashSet<Driver> friends, ref HashSet<Driver> flock)
+		{
+			flock.Clear();
+			friends.Clear();
+			foreach (var neighbor in drivers)
+			{
+				if (driver != neighbor)
+				{
+					var distSq = (driver.transform.position - neighbor.transform.position).sqrMagnitude; 
+					if (distSq <= friendRadiusSq)
+					{
+						friends.Add(neighbor);
+					}
+					else if (distSq <= flockRadiusSq)
+					{
+						flock.Add(neighbor);
+					}
+				}
+			}
+		}
+	}
+}
